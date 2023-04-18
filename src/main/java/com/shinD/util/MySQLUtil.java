2e8eaf4c43ba70004aa5b1f0b907d22714876ce8
e-7281998 <jejjcs43@naver.com>
2023-04-18 21:13:03 +0900

    [DB 연결] mysql 연결

--- src/main/java/com/shinD/util/MySQLUtil.java
+++ src/main/java/com/shinD/util/MySQLUtil.java
@@ -13,26 +13,18 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class MySQLUtil {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
- 		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println("1. driver load 성공"); 
-		
-		String url = "jdbc:mysql://127.0.0.1/ShinD";
-		String userid="ShinD", pass="1234";
-		Connection conn = DriverManager.getConnection(url, userid, pass);
-		System.out.println("2. Connection 성공");
-		  
-	}
 	
  	public static Connection getConnection() {
+ 		Connection conn = null;
+		String url = "jdbc:mysql://127.0.0.1/ShinD";
+		String userid="ShinD", pass="1234";
+
 		Context initContext;
-		Connection conn = null;
 		try {
-			initContext = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			DataSource ds = (DataSource)envContext.lookup("jdbc/sqlserver");
- 			 conn = ds.getConnection();
- 		} catch (NamingException e) {
+	 		Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, userid, pass);
+	 		 
+ 		} catch (ClassNotFoundException e) {
  			e.printStackTrace();
 		}catch (SQLException e) {
  			e.printStackTrace();
