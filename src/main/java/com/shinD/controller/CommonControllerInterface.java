c6900b0fe1defd08731417e244381cde04a0bd64
e-7281998 <jejjcs43@naver.com>
2023-04-18 11:01:53 +0900

    [CommonControllerInterface] 생성

---
+++ src/main/java/com/shinD/controller/CommonControllerInterface.java
@@ -0,0 +1,8 @@
+package com.shinD.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+ public interface CommonControllerInterface {
+ 	public String execute(Map<String, Object> data) throws Exception;
+}
