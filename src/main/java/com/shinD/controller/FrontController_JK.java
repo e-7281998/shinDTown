169f30412b57e2ae67bf2b5adb186c87d3f71af0
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-04-19 12:26:56 +0900

    [지만]하나 잘못올려서 되돌림

--- src/main/java/com/shinD/controller/FrontController_JK.java
+++ src/main/java/com/shinD/controller/FrontController_JK.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
  
-@WebServlet("/2")
+@WebServlet("/*")
 public class FrontController_JK extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
