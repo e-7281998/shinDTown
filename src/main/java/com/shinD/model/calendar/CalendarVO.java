65c1a84bfedaa694412288bf3396da9d37fea2d8
Your Name <90436@naver.com>
2023-04-20 17:40:42 +0900

    [캘린더] VO 생성

--- src/main/java/com/shinD/model/calendar/CalendarVO.java
+++ src/main/java/com/shinD/model/calendar/CalendarVO.java
@@ -1,5 +1,24 @@
 package com.shinD.model.calendar;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CalendarVO {
+	
+	private int plan_code;
+	private String plan_title;
+	private String plan_content;
+	private Date start_date;
+	private Date end_date;
 
 }
