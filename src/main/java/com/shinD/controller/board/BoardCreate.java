81772aea21f7f1a6417309167056b27c478e7a8a
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-04-21 12:05:30 +0900

    [게시판]게시판 생성

--- src/main/java/com/shinD/controller/board/BoardCreate.java
+++ src/main/java/com/shinD/controller/board/BoardCreate.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.shinD.controller.CommonControllerInterface;
+import com.shinD.model.board.BoardVO;
 
 
 
@@ -18,45 +19,49 @@ public class BoardCreate implements CommonControllerInterface {
 	@Override
 	public String execute(Map<String, Object> data) throws Exception{
 		HttpServletRequest request = (HttpServletRequest) data.get("request");//입력받은 값 가져오기
+		
 		request.setCharacterEncoding("utf-8");//인코딩
-		String page = "BoardCreate.jsp";
+		String page = "../view/boardView/BoardCreate.jsp";
 		String method = (String)data.get("method");
+		BoardService bservice = new BoardService();
 		
 		//메소드 형식 따라서
 		if(method.equals("POST")) {
 			//보드 중복체크
-			BoardService bservice = new BoardService();
+			
 			int result = bservice.boardCheck(request.getParameter("board_name"));//1이면 중복 0이면 없는 게시판 명
 	
 			//보드 생성 게시판명이 유니크여서 오류가 뜨지만 뜨기전에 해결
 			if(result == 1) {//게시판이 이미 존재
 				System.out.println("해당 게시판명은 사용중입니다");
 			}else {//게시판이 없다
-				BoardVO board = makeBoard(request);//보드형식에 입력받은 값을 넣고 새로운 보드를 만든다
-				bservice.BoardCreate(board);//위에서 만든 보드형식으로 새로운 보드 만들기
+				HttpSession session = request.getSession();
+				
+				
+				bservice.BoardCreate((Integer)session.getAttribute("user_code"),request.getParameter("board_name"));//위에서 만든 보드형식으로 새로운 보드 만들기
 				System.out.println("생성 완료햇습니다.");
-				bservice.boardAll();//테스트겸 보드 목록보기
+				
+				page = "redirect:/shinDTown/board/read.jm";
 			}
-			page = "responseBody:게시판생성완료";
 			
-		}else {}
+		}else {return page;}
 		
 		return page;
 	}
 
-	//보드만들기
-	private BoardVO makeBoard(HttpServletRequest request) throws UnsupportedEncodingException {
-		request.setCharacterEncoding("utf-8");
-		//int board_code = Integer.parseInt(request.getParameter("board_code"));오토라서 주석처리함
-		int user_code = Integer.parseInt(request.getParameter("user_code"));
-		String board_name = request.getParameter("board_name");
-		
-		BoardVO board = new BoardVO();
-		//board.setBOARD_CODE(board_code);오토라서 주석처리함
-		board.setUSER_CODE(user_code);
-		board.setBOARD_NAME(board_name);
-		
-		
-		return board;
-	}
+//	//보드만들기
+//	private BoardVO makeBoard(HttpServletRequest request) throws UnsupportedEncodingException {
+//		request.setCharacterEncoding("utf-8");
+//		//int board_code = Integer.parseInt(request.getParameter("board_code"));오토라서 주석처리함
+//		int user_code = Integer.parseInt(request.getParameter("user_code"));
+//		String board_name = request.getParameter("board_name");
+//		
+//		BoardVO board = new BoardVO();
+//		//board.setBOARD_CODE(board_code);오토라서 주석처리함
+//		board.setUSER_CODE(1);
+//		board.setBOARD_NAME(board_name);
+//		
+//		
+//		return board;
+//	}
 }
--- src/main/webapp/view/boardView/BoardCreate.jsp
+++ src/main/webapp/view/boardView/BoardCreate.jsp
@@ -1,28 +1,33 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	<%@ taglib prefix="c"  uri="http://java.sun.com/jsp/jstl/core"%>
+ <c:set var="path" value="${pageContext.request.contextPath }" scope="application"></c:set>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<link href="board.css" rel="stylesheet" />
-<script src="../../jq/jquery-3.6.4.min.js"></script>
+<link href="${path}/view/boardView/board.css" rel="stylesheet" />
+<script src="${path}/jq/jquery-3.6.4.min.js"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body class="main">
 	<%@include file="../header.jsp"%>
 	<div class="body">
 		<div class="board_page_2">
-			<form class="posting_form">
+			<form class="posting_form" action = "/shinDTown/board/create.jm" method="POST">
 			
 				<div class="new_title">
 					<label class="label_1">게시판명</label> 
-					<input type="text" name="title" class="title" id="title" />
-						<button class="dup_check" onclick="location.href=''">중복확인</button>
+					<input type="text" name="board_name" id="title" />
+					<input type="button" id="dup_check" value="중복체크" />
+						<!-- <button class="dup_check">중복확인</button> -->
 				</div>
 
 				<div class="new_pre">
 					<label class="label_2">게시판 설명</label> 
-					<input type="text" name="content" class="content" />
+					<input type="text" name="content" id="content" />
 				</div>
 
 				<div class="board_btn">
@@ -34,6 +39,32 @@
 
 		</div>
 	</div>
+	<script>
+$(function(){
+		$("#dup_check").on("click",function(){
+			//page이동 없이 서버에 요청보내고 응답받기 :ajax
+		$.ajax({
+			url:"/shinDTown/board/dupcheck.jm",
+			data:{"board_name":$("#title").val()},
+			success:function(responseData){
+				$("#message").text(responseData);
+				if(responseData==1){
+					alert("사용이 불가능합니다");
+				
+				}
+				else if(responseData==0){
+					alert("사용이 가능합니다");
+					
+				}
+			},
+			error:function(message){
+				$("#message").text(message);
+				
+			}
+		});
+		})
+	});
+	</script>
 	<script>
 		$(document).ready(function() {
 			let p_toggle = true;
