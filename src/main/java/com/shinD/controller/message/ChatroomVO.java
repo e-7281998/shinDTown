caf1030a3dcc362cbfad24799b522d9c9b6da32f
JinGyeong <wlsrud677@naver.com>
2023-04-24 10:10:19 +0900

    [채팅방] 채팅방, 상대방의 이름과 코드가 있는 채팅방VO

---
+++ src/main/java/com/shinD/controller/message/ChatroomVO.java
@@ -0,0 +1,20 @@
+package com.shinD.controller.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ChatroomVO {
+	private int chat_code;
+	private int friend_code;
+	
+	private String friend_name;
+	
+}
