6672b18d610941ac461a14f23414a1525d2322cb
JinGyeong <wlsrud677@naver.com>
2023-04-24 10:11:49 +0900

    [채팅방] 메시지VO

---
+++ src/main/java/com/shinD/controller/message/MessageVO.java
@@ -0,0 +1,23 @@
+package com.shinD.controller.message;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MessageVO {
+	 private int message_code;
+	 private int chat_code;
+	 private int sender;
+	 private Date message_create;
+	 private String message_data;
+	 private boolean message_open;
+}
