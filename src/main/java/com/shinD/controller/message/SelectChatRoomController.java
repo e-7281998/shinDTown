905519ed7009d09b35907d3e15051fcd7decf736
JinGyeong <wlsrud677@naver.com>
2023-04-24 10:10:57 +0900

    [채팅방] 내가 속한 채팅방 목록 조회가 가능한 컨트롤러

---
+++ src/main/java/com/shinD/controller/message/SelectChatRoomController.java
@@ -0,0 +1,36 @@
+package com.shinD.controller.message;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.shinD.controller.CommonControllerInterface;
+import com.shinD.model.message.MessageService;
+
+public class SelectChatRoomController implements CommonControllerInterface{
+	//내가 속한 채팅방목록
+	@Override
+	public String execute(Map<String, Object> data) throws Exception {
+
+		HttpServletRequest request = (HttpServletRequest) data.get("request");//입력받은 값 가져오기
+		request.setCharacterEncoding("utf-8");//인코딩
+		String method = (String)data.get("method");
+		String page = "";
+		
+		HttpSession session = request.getSession();
+		int user_code = (int)(session.getAttribute("user_code"));
+		
+		MessageService mService = new  MessageService();
+	
+		List<ChatroomVO> chatRoomlist = mService.selectChatRoom(user_code);
+		
+		request.setAttribute("chatRoomList", chatRoomlist);//보드리스트 값 리퀘스트에넣기
+	
+		
+		//포워드로 보내기 why? request값을 넘겨줘야하기 때문에
+		return "/view/chatView/ChatRead.jsp";
+	}
+	
+}
