bb6517f5cde92e2530dfe08626abbd8377e9c845
e-7281998 <jejjcs43@naver.com>
2023-04-26 12:29:43 +0900

    [필터]로그인 체크 필터 생성

---
+++ src/main/java/com/shinD/filter/LoginCheckFilter.java
@@ -0,0 +1,54 @@
+package com.shinD.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("*.com")
+public class LoginCheckFilter extends HttpFilter implements Filter {
+
+	public LoginCheckFilter() {
+		super();
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		
+		 if(req.getServletPath().equals( "/view/memberView/signup.com") ||
+			 req.getServletPath().equals("/view/memberView/idDupCheck.com") ||
+			 req.getServletPath().equals("/view/memberView/classCheck.com") ||
+			 req.getServletPath().equals("/view/memberView/MemberSignUp.com")||
+			 req.getServletPath().equals("/view/memberView/login.com")) {
+		 
+		 }else { HttpSession session = req.getSession();
+		 
+			 if(session.getAttribute("loginUser") == null) {
+				 res.sendRedirect(req.getContextPath()+"/index.jsp");
+				 return;
+			}
+		 }
+		 
+
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
