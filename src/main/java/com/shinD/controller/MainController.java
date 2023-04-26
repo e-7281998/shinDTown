b0cdf4788e0359a1a5614102c4fc2559f4a2cd99
e-7281998 <jejjcs43@naver.com>
2023-04-26 11:50:20 +0900

    [메인컨트롤러] 진경, 유진, 지만 추가

--- src/main/java/com/shinD/controller/MainController.java
+++ src/main/java/com/shinD/controller/MainController.java
@@ -11,12 +11,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.shinD.controller.board.BoardCreate;
+import com.shinD.controller.board.BoardDelete;
+import com.shinD.controller.board.BoardDetail;
+import com.shinD.controller.board.BoardDupCheck;
+import com.shinD.controller.board.BoardMain;
+import com.shinD.controller.board.BoardRead;
+import com.shinD.controller.comment.CommentCreate;
+import com.shinD.controller.comment.CommentList;
 import com.shinD.controller.git.Git;
 import com.shinD.controller.member.MemberLogin;
 import com.shinD.controller.member.MemberLogout;
 import com.shinD.controller.member.MemberSignUp;
 import com.shinD.controller.member.MemberUpdate;
 import com.shinD.controller.member.MemberWithdraw;
+import com.shinD.controller.message.InsertMessageController;
+import com.shinD.controller.message.MessageToConnectMemberController;
+import com.shinD.controller.message.ReadConnectRoomController;
+import com.shinD.controller.message.SelectChatRoomController;
+import com.shinD.controller.plan.CreatePlan;
+import com.shinD.controller.plan.DeletePlan;
+import com.shinD.controller.plan.DetailPlan;
+import com.shinD.controller.plan.ReadPlan;
+import com.shinD.controller.post.PostCreate;
+import com.shinD.controller.post.PostLike;
  
 @WebServlet("*.com")
 public class MainController extends HttpServlet {
@@ -31,8 +49,11 @@ public class MainController extends HttpServlet {
 		Map<String, Object> data = new HashMap<>();
 		data.put("method", request.getMethod());
 		data.put("request", request); 
+		data.put("response", response);
+
 		
 		switch (path) { 
+		//은정
 		case "/view/memberView/signup.com":
 		case "/view/memberView/idDupCheck.com":
 		case "/view/memberView/classCheck.com":
@@ -57,6 +78,74 @@ public class MainController extends HttpServlet {
 		case "/view/gitView/gitUpdate.com":
  			controller = new Git();
 			 break; 
+		//지만
+		case "/board/main.com":
+			controller = new BoardMain();
+			break;
+		case "/board/create.com":
+			controller = new BoardCreate();
+			break;
+		case "/board/read.com":
+			controller = new BoardRead();
+			break;
+		case "/board/delete.com":
+			controller = new BoardDelete();
+			break;
+		case "/board/dupcheck.com":
+			controller = new BoardDupCheck();
+			break;
+		case "/board/detail.com":
+			controller = new BoardDetail();
+			break;
+		case "/post/create.com":
+			controller = new PostCreate();
+			break;
+		case "/post/like.com":
+			controller = new PostLike();
+			break;
+		case "/comment/create.com":
+			controller = new CommentCreate();
+			break;
+		case "/comment/list.com":
+			System.out.println("------------");
+			controller = new CommentList();
+			break;
+		//유진
+		case "/view/calendarView/ReadPlan.com":
+			controller = new ReadPlan();
+			break;
+		case "/view/calendarView/CreatePlan.com":
+			controller = new CreatePlan();
+			break;
+		case "/view/calendarView/DetailPlan.com" : 
+			controller  = new DetailPlan();
+			break;
+		case "/view/calendarView/DeletePlan.com" : 
+			controller = new DeletePlan();
+			break;
+		//진경
+		case "/view/chatView/selectChatRoom.com":
+			controller = new SelectChatRoomController();
+			break;
+		case "/view/chatView/insertMessage.com":
+ 			controller = new InsertMessageController();
+			 break;
+		case "/view/chatView/messageToConnectMember.com":
+ 			controller = new MessageToConnectMemberController();
+			 break;
+		case "/view/chatView/readConnectRoom.com":
+ 			controller = new ReadConnectRoomController();
+			 break;
+		case "/view/chatView/selectReceiveMessage.com":
+ 			controller = new SelectReceiveMessageController();
+ 			break;
+		case "/view/chatView/selectNotReadMessage.com":
+ 			controller = new SelectNotReadMessageController();
+			 break;	 
+		case "/view/chatView/selectReadOneMessage.com":
+ 			controller = new SelectReadOneMessageController();
+			 break;	 
+		
 		default:
 			break;
 		}
